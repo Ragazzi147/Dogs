659cd6e443fbae5859d15678b60bd265570e02bc
Lucas <lucasax147@gmail.com>
2024-10-28 23:34:05 -0300

    Login Validate Tokne and AutoLogin

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import LoginCreate from './LoginCreate ';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
 import LoginForm from './LoginForm';
+import { UserContext } from '../../UserContext';
 
 const Login = () => {
+  const { login } = React.useContext(UserContext);
+
+  if (login === true) return <Navigate to="/conta" />;
   return (
     <div>
       <Routes>
