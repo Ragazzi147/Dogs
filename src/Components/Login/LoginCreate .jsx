840735773fdc04df24255bc0a4d2e85178ee0a15
Lucas <lucasax147@gmail.com>
2024-10-29 01:10:25 -0300

    login create form e crud

--- src/Components/Login/LoginCreate .jsx
+++ src/Components/Login/LoginCreate .jsx
@@ -2,14 +2,25 @@ import React from 'react';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
+import { USER_POST } from '../../api';
+import { UserContext } from '../../UserContext';
 
 const LoginCreate = () => {
   const username = useForm();
   const email = useForm('email');
-  const password = useForm('password');
+  const password = useForm();
 
-  function handleSubmit(event) {
+  const { userLogin } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
     event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    if (response.ok) userLogin(username.value, password.value);
   }
 
   return (
