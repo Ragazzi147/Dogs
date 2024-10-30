f8dc240e81123024c3f43c0fb474c0b7d8945d7a
Lucas <lucasax147@gmail.com>
2024-10-30 00:36:54 -0300

    UserContext navigate

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -9,7 +9,7 @@ export const UserStorage = ({ children }) => {
   const [login, setLogin] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
-
+  const navigate = useNavigate();
   const userLogout = React.useCallback(async function () {
     setData(null);
     setError(null);
