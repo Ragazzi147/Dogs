dfc257b3738797d659394e921f69523eeaf95d48
Lucas <lucasax147@gmail.com>
2024-11-27 13:22:49 -0300

    Infinite Scroll

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -4,8 +4,11 @@ import { Route, Routes } from 'react-router-dom';
 import Feed from '../Feed/Feed';
 import UserPhotoPost from './UserPhotoPost';
 import UserStats from './UserStats';
+import { UserContext } from '../../UserContext';
 
 const User = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <section className="container">
       <UserHeader />
