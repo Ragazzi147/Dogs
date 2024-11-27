b7b637fda891a6fb34df6a7f2af375fbadf06af7
Lucas <lucasax147@gmail.com>
2024-11-27 14:07:52 -0300

    page photo

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from './Components/Login/Login';
 import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import Photo from './Components/Photo/Photo';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
+
             <Route
               path="conta/*"
               element={
@@ -26,6 +28,7 @@ const App = () => {
                 </ProtectedRoute>
               }
             />
+            <Route path="foto/:id" element={<Photo />} />
           </Routes>
           <Footer />
         </UserStorage>
